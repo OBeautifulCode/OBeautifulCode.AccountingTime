219d642a04b97c61d910b30e813f5f0043258560
Suraj K. Gupta <surajg@gmail.com>
2016-11-28 19:09:59 -0500

    added ReportingPeriodExtensions

---
+++ OBeautifulCode.AccountingTime/Extensions/ReportingPeriodExtensions.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ReportingPeriodExtensions.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable CheckNamespace
+namespace OBeautifulCode.AccountingTime
+{
+    /// <summary>
+    /// Extension methods on <see cref="ReportingPeriod{T}"/>.
+    /// </summary>
+    public static class ReportingPeriodExtensions
+    {
+    }
+}
+
+// ReSharper restore CheckNamespace
