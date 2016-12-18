0a5ba8a40fd79f69a8b542f27aa4b6756905cb70
Suraj K. Gupta <surajg@gmail.com>
2016-12-18 14:38:00 -0500

    add ReportingPeriodComponent

---
+++ OBeautifulCode.AccountingTime/ReportingPeriod/ReportingPeriodComponent.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ReportingPeriodComponent.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.AccountingTime.ReportingPeriod
+{
+    /// <summary>
+    /// Represents the start, end, or both, of a reporting period.
+    /// </summary>
+    public enum ReportingPeriodComponent
+    {
+        /// <summary>
+        /// Invalid reporting period component.
+        /// </summary>
+        /// <remarks>
+        /// This is required so that there is a default value for the enum.
+        /// </remarks>
+        Invalid,
+
+        /// <summary>
+        /// The start of a reporting period.
+        /// </summary>
+        Start,
+
+        /// <summary>
+        /// The end of a reporting period.
+        /// </summary>
+        End,
+
+        /// <summary>
+        /// Both the start and the end of a reporting period.
+        /// </summary>
+        Both
+    }
+}
