426f01ed58d2f494fc543bc182b690e3ea6ace93
Suraj K. Gupta <surajg@gmail.com>
2016-12-21 20:29:08 -0500

    clean up documentation

--- OBeautifulCode.AccountingTime/UnitOfTime/FiscalYear.cs
+++ OBeautifulCode.AccountingTime/UnitOfTime/FiscalYear.cs
@@ -167,18 +167,7 @@ namespace OBeautifulCode.AccountingTime
             return this.Year.CompareTo(other.Year);
         }
 
-        /// <summary>
-        /// Compares the current instance with another object and returns an integer
-        /// that indicates whether the current instance precedes,
-        /// follows, or occurs in the same position in the sort order as the other object
-        /// </summary>
-        /// <param name="obj">An object to compare to this instance.</param>
-        /// <returns>
-        /// -1 if the current instance is less than other.
-        /// 0 if the current instance is equal to the other.
-        /// 1 if the current instance is greater than the other.
-        /// </returns>
-        /// <exception cref="ArgumentException"><paramref name="obj"/> is not of type <see cref="FiscalYear"/>.</exception>
+        /// <inheritdoc />
         public override int CompareTo(object obj)
         {
             var other = obj as FiscalYear;
--- OBeautifulCode.AccountingTime/UnitOfTime/GenericMonth.cs
+++ OBeautifulCode.AccountingTime/UnitOfTime/GenericMonth.cs
@@ -180,18 +180,7 @@ namespace OBeautifulCode.AccountingTime
             return thisDay.CompareTo(otherDay);
         }
 
-        /// <summary>
-        /// Compares the current instance with another object and returns an integer
-        /// that indicates whether the current instance precedes,
-        /// follows, or occurs in the same position in the sort order as the other object
-        /// </summary>
-        /// <param name="obj">An object to compare to this instance.</param>
-        /// <returns>
-        /// -1 if the current instance is less than other.
-        /// 0 if the current instance is equal to the other.
-        /// 1 if the current instance is greater than the other.
-        /// </returns>
-        /// <exception cref="ArgumentException"><paramref name="obj"/> is not of type <see cref="GenericMonth"/>.</exception>
+        /// <inheritdoc />
         public override int CompareTo(object obj)
         {
             var other = obj as GenericMonth;
