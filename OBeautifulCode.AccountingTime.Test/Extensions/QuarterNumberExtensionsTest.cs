4acb7ada91e511f861264d82fb9fbc239658b21b
Suraj K. Gupta <surajg@gmail.com>
2017-01-04 08:51:50 -0500

    added QuarterNumber extension methods to create Calendar/Fiscal/Generic Quarters

---
+++ OBeautifulCode.AccountingTime.Test/Extensions/QuarterNumberExtensionsTest.cs
@@ -0,0 +1,96 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="QuarterNumberExtensionsTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable CheckNamespace
+namespace OBeautifulCode.AccountingTime.Test
+{
+    using System;
+
+    using AutoFakeItEasy;
+
+    using FakeItEasy;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public static class QuarterNumberExtensionsTest
+    {
+        // ReSharper disable InconsistentNaming
+        [Fact]
+        public static void ToCalendar___Should_throw_ArgumentException___When_parameter_quarterNumber_is_Invalid()
+        {
+            // Arrange, Act
+            var ex = Record.Exception(() => QuarterNumber.Invalid.ToCalendar(A.Dummy<PositiveInteger>().ThatIs(y => y < 10000)));
+
+            // Assert
+            ex.Should().BeOfType<ArgumentException>();
+        }
+
+        [Fact]
+        public static void ToCalendar___Should_return_CalendarQuarter_with_specified_quarterNumber_and_year___When_called()
+        {
+            // Arrange
+            var expectedQuarter = A.Dummy<CalendarQuarter>();
+
+            // Act
+            var actualQuarter = expectedQuarter.QuarterNumber.ToCalendar(expectedQuarter.Year);
+
+            // Assert
+            actualQuarter.Should().Be(expectedQuarter);
+        }
+
+        [Fact]
+        public static void ToFiscal___Should_throw_ArgumentException___When_parameter_quarterNumber_is_Invalid()
+        {
+            // Arrange, Act
+            var ex = Record.Exception(() => QuarterNumber.Invalid.ToFiscal(A.Dummy<PositiveInteger>().ThatIs(y => y < 10000)));
+
+            // Assert
+            ex.Should().BeOfType<ArgumentException>();
+        }
+
+        [Fact]
+        public static void ToFiscal___Should_return_FiscalQuarter_with_specified_quarterNumber_and_year___When_called()
+        {
+            // Arrange
+            var expectedQuarter = A.Dummy<FiscalQuarter>();
+
+            // Act
+            var actualQuarter = expectedQuarter.QuarterNumber.ToFiscal(expectedQuarter.Year);
+
+            // Assert
+            actualQuarter.Should().Be(expectedQuarter);
+        }
+
+        [Fact]
+        public static void ToGeneric___Should_throw_ArgumentException___When_parameter_quarterNumber_is_Invalid()
+        {
+            // Arrange, Act
+            var ex = Record.Exception(() => QuarterNumber.Invalid.ToGeneric(A.Dummy<PositiveInteger>().ThatIs(y => y < 10000)));
+
+            // Assert
+            ex.Should().BeOfType<ArgumentException>();
+        }
+
+        [Fact]
+        public static void ToGeneric___Should_return_GenericQuarter_with_specified_quarterNumber_and_year___When_called()
+        {
+            // Arrange
+            var expectedQuarter = A.Dummy<GenericQuarter>();
+
+            // Act
+            var actualQuarter = expectedQuarter.QuarterNumber.ToGeneric(expectedQuarter.Year);
+
+            // Assert
+            actualQuarter.Should().Be(expectedQuarter);
+        }
+
+        // ReSharper restore InconsistentNaming
+    }
+}
+
+// ReSharper restore CheckNamespace
---
+++ OBeautifulCode.AccountingTime/Extensions/QuarterNumberExtensions.cs
@@ -0,0 +1,78 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="QuarterNumberExtensions.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable CheckNamespace
+namespace OBeautifulCode.AccountingTime
+{
+    using System;
+
+    using static System.FormattableString;
+
+    /// <summary>
+    /// Extension methods on type <see cref="UnitOfTimeGranularity"/>.
+    /// </summary>
+    public static class QuarterNumberExtensions
+    {
+        /// <summary>
+        /// Constructs a calendar quarter from a <see cref="QuarterNumber"/> and a year.
+        /// </summary>
+        /// <param name="quarterNumber">The quarter number.</param>
+        /// <param name="year">The year.</param>
+        /// <returns>
+        /// A calendar quarter constructed from the specified <see cref="QuarterNumber"/> and year.
+        /// </returns>
+        public static CalendarQuarter ToCalendar(this QuarterNumber quarterNumber, int year)
+        {
+            if (quarterNumber == QuarterNumber.Invalid)
+            {
+                throw new ArgumentException(Invariant($"{nameof(quarterNumber)} is {nameof(QuarterNumber.Invalid)}"));
+            }
+
+            var result = new CalendarQuarter(year, quarterNumber);
+            return result;
+        }
+
+        /// <summary>
+        /// Constructs a fiscal quarter from a <see cref="QuarterNumber"/> and a year.
+        /// </summary>
+        /// <param name="quarterNumber">The quarter number.</param>
+        /// <param name="year">The year.</param>
+        /// <returns>
+        /// A fiscal quarter constructed from the specified <see cref="QuarterNumber"/> and year.
+        /// </returns>
+        public static FiscalQuarter ToFiscal(this QuarterNumber quarterNumber, int year)
+        {
+            if (quarterNumber == QuarterNumber.Invalid)
+            {
+                throw new ArgumentException(Invariant($"{nameof(quarterNumber)} is {nameof(QuarterNumber.Invalid)}"));
+            }
+
+            var result = new FiscalQuarter(year, quarterNumber);
+            return result;
+        }
+
+        /// <summary>
+        /// Constructs a generic quarter from a <see cref="QuarterNumber"/> and a year.
+        /// </summary>
+        /// <param name="quarterNumber">The quarter number.</param>
+        /// <param name="year">The year.</param>
+        /// <returns>
+        /// A generic quarter constructed from the specified <see cref="QuarterNumber"/> and year.
+        /// </returns>
+        public static GenericQuarter ToGeneric(this QuarterNumber quarterNumber, int year)
+        {
+            if (quarterNumber == QuarterNumber.Invalid)
+            {
+                throw new ArgumentException(Invariant($"{nameof(quarterNumber)} is {nameof(QuarterNumber.Invalid)}"));
+            }
+
+            var result = new GenericQuarter(year, quarterNumber);
+            return result;
+        }
+    }
+}
+
+// ReSharper restore CheckNamespace
