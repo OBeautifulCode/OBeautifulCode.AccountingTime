4b05ee41cc598dee938020e0a2eb6ef0204dc5d8
Suraj K. Gupta <surajg@gmail.com>
2017-02-26 14:02:39 -0500

    only do one try/catch

--- OBeautifulCode.AccountingTime.BsonSerialization/AccountingTimeSerializer.cs
+++ OBeautifulCode.AccountingTime.BsonSerialization/AccountingTimeSerializer.cs
@@ -30,57 +30,51 @@ namespace OBeautifulCode.AccountingTime.BsonSerialization
         /// </summary>
         public static void Register()
         {
-            // register various flavors of UnitOfTime
-            var unitOfTimeType = typeof(UnitOfTime);
-            var unitOfTimeTypesToRegister =
-                unitOfTimeType.Assembly.GetTypes()
-                    .Where(type => (type == unitOfTimeType) || type.IsSubclassOf(unitOfTimeType))
-                    .ToList();
-            unitOfTimeTypesToRegister.ForEach(
-                t =>
-                {
-                    try
-                    {
-                        BsonSerializer.RegisterSerializer(
-                            t, 
-                            Activator.CreateInstance(typeof(UnitOfTimeSerializer<>).MakeGenericType(t)) as IBsonSerializer);
-                    }
-                    catch (BsonSerializationException)
-                    {
-                        // get rid of this nonsense when this method is available:
-                        // https://github.com/mongodb/mongo-csharp-driver/pull/264
-                    }
-                });
-
-            // register various flavors of IReportingPeriod
-            var reportingPeriodType = typeof(IReportingPeriod<>);
-            var reportingPeriodTypesToRegister =
-                reportingPeriodType.Assembly.GetTypes()
-                    .Where(type =>
-                        (type == reportingPeriodType) ||
-                        type.GetInterfaces()
-                            .Where(_ => _.IsGenericType)
-                            .Select(_ => _.GetGenericTypeDefinition())
-                            .Contains(reportingPeriodType))
-                    .ToList();
-
-            foreach (var reportingPeriodTypeToRegister in reportingPeriodTypesToRegister)
+            try
             {
+                // register various flavors of UnitOfTime
+                var unitOfTimeType = typeof(UnitOfTime);
+                var unitOfTimeTypesToRegister =
+                    unitOfTimeType.Assembly.GetTypes()
+                        .Where(type => (type == unitOfTimeType) || type.IsSubclassOf(unitOfTimeType))
+                        .ToList();
                 unitOfTimeTypesToRegister.ForEach(
                     t =>
                     {
-                        try
+                        BsonSerializer.RegisterSerializer(t,
+                            Activator.CreateInstance(typeof(UnitOfTimeSerializer<>).MakeGenericType(t)) as
+                                IBsonSerializer);
+                    });
+
+                // register various flavors of IReportingPeriod
+                var reportingPeriodType = typeof(IReportingPeriod<>);
+                var reportingPeriodTypesToRegister =
+                    reportingPeriodType.Assembly.GetTypes()
+                        .Where(type =>
+                            (type == reportingPeriodType) ||
+                            type.GetInterfaces()
+                                .Where(_ => _.IsGenericType)
+                                .Select(_ => _.GetGenericTypeDefinition())
+                                .Contains(reportingPeriodType))
+                        .ToList();
+
+                foreach (var reportingPeriodTypeToRegister in reportingPeriodTypesToRegister)
+                {
+                    unitOfTimeTypesToRegister.ForEach(
+                        t =>
                         {
                             BsonSerializer.RegisterSerializer(
                                 reportingPeriodTypeToRegister.MakeGenericType(t),
-                                Activator.CreateInstance(typeof(IReportingPeriodSerializer<>).MakeGenericType(reportingPeriodTypeToRegister.MakeGenericType(t))) as IBsonSerializer);
-                        }
-                        catch (BsonSerializationException)
-                        {
-                            // get rid of this nonsense when this method is available:
-                            // https://github.com/mongodb/mongo-csharp-driver/pull/264
-                        }
-                    });
+                                Activator.CreateInstance(
+                                    typeof(IReportingPeriodSerializer<>).MakeGenericType(
+                                        reportingPeriodTypeToRegister.MakeGenericType(t))) as IBsonSerializer);
+                        });
+                }
+            }
+            catch (BsonSerializationException)
+            {
+                // get rid of this nonsense when this method is available:
+                // https://github.com/mongodb/mongo-csharp-driver/pull/264
             }
         }
     }
