4d4fc7386fe261a91bf7a80b6b5cd18455dc9c4b
Suraj Gupta <SurajGupta@users.noreply.github.com>
2022-01-13 12:21:26 -0500

    added UnitOfTimeKind.ToUnbounded()

---
+++ OBeautifulCode.AccountingTime.Test/Logic/Extensions/UnitOfTimeKindExtensionsTest.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UnitOfTimeKindExtensionsTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.AccountingTime.Test
+{
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Enum.Recipes;
+    using Xunit;
+
+    public static class UnitOfTimeKindExtensionsTest
+    {
+        [Fact]
+        public static void ToUnbounded___Should_throw_ArgumentOutOfRangeException___When_parameter_unitOfTimeKind_is_Invalid()
+        {
+            // Arrange, Act
+            var ex = Record.Exception(() => UnitOfTimeKind.Invalid.ToUnbounded());
+
+            // Assert
+            ex.Should().BeOfType<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public static void ToUnbounded___Should_return_specified_kind_of_unbounded_unit_of_time___When_called()
+        {
+            // Arrange
+            var unitOfTimeKinds = EnumExtensions.GetDefinedEnumValues<UnitOfTimeKind>().Except(new[] { UnitOfTimeKind.Invalid }).ToList();
+
+            // Act
+            var actual = unitOfTimeKinds.Select(_ => new { UnitOfTimeKind = _, UnitOfTime = _.ToUnbounded() }).ToList();
+
+            // Assert
+            actual.Select(_ => _.UnitOfTime.UnitOfTimeGranularity).AsTest().Must().Each().BeEqualTo(UnitOfTimeGranularity.Unbounded);
+            actual.Select(_ => _.UnitOfTimeKind).AsTest().Must().BeEqualTo(actual.Select(_ => _.UnitOfTime.UnitOfTimeKind));
+        }
+    }
+}
---
+++ OBeautifulCode.AccountingTime/Logic/Extensions/UnitOfTimeKindExtensions.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UnitOfTimeKindExtensions.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.AccountingTime
+{
+    using System;
+
+    using static System.FormattableString;
+
+    /// <summary>
+    /// Extension methods on type <see cref="UnitOfTimeKind"/>.
+    /// </summary>
+    public static class UnitOfTimeKindExtensions
+    {
+        /// <summary>
+        /// Constructs an unbounded unit-of-time of the specified kind.
+        /// </summary>
+        /// <param name="unitOfTimeKind">The kind of unit-of-time.</param>
+        /// <returns>
+        /// The unbounded unit-of-time.
+        /// </returns>
+        /// <exception cref="ArgumentException"><paramref name="unitOfTimeKind"/> is <see cref="UnitOfTimeKind.Invalid"/>.</exception>
+        public static UnitOfTime ToUnbounded(
+            this UnitOfTimeKind unitOfTimeKind)
+        {
+            if (unitOfTimeKind == UnitOfTimeKind.Invalid)
+            {
+                throw new ArgumentOutOfRangeException(Invariant($"'{nameof(unitOfTimeKind)}' == '{UnitOfTimeKind.Invalid}'"), (Exception)null);
+            }
+
+            UnitOfTime result;
+
+            switch (unitOfTimeKind)
+            {
+                case UnitOfTimeKind.Calendar:
+                    result = new CalendarUnbounded();
+                    break;
+                case UnitOfTimeKind.Fiscal:
+                    result = new FiscalUnbounded();
+                    break;
+                case UnitOfTimeKind.Generic:
+                    result = new GenericUnbounded();
+                    break;
+                default:
+                    throw new NotSupportedException(Invariant($"This {nameof(UnitOfTimeKind)} is not supported: {unitOfTimeKind}."));
+            }
+
+            return result;
+        }
+    }
+}
