cf2533a115d650c96b79a6d432b7d5d28c2b0696
Suraj Gupta <SurajGupta@users.noreply.github.com>
2024-11-18 15:27:09 -0500

    Add Previous extension method to get previous CalendarUnitOfTime

--- OBeautifulCode.AccountingTime.Test/Logic/Extensions/DateTimeExtensionsTest.cs
+++ OBeautifulCode.AccountingTime.Test/Logic/Extensions/DateTimeExtensionsTest.cs
@@ -7,9 +7,10 @@
 namespace OBeautifulCode.AccountingTime.Test
 {
     using System;
-
+    using System.Linq;
+    using FakeItEasy;
     using FluentAssertions;
-
+    using OBeautifulCode.Assertion.Recipes;
     using Xunit;
 
     public static class DateTimeExtensionsTest
@@ -39,6 +40,358 @@ namespace OBeautifulCode.AccountingTime.Test
             actualCalendarDay2.Should().Be(expectedCalendarDay2);
             actualCalendarDay3.Should().Be(expectedCalendarDay3);
             actualCalendarDay4.Should().Be(expectedCalendarDay4);
+        }
+
+        [Fact]
+        public static void Previous___Should_throw_ArgumentOutOfRangeException___When_parameter_granularity_is_Unbounded()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => A.Dummy<DateTime>().Previous(UnitOfTimeGranularity.Unbounded));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentOutOfRangeException>();
+            actual.Message.AsTest().Must().ContainString("granularity");
+        }
+
+        [Fact]
+        public static void Previous___Should_return_previous_UnitOfTime___When_called()
+        {
+            // Arrange
+            var valueAndExpected = new[]
+            {
+                new
+                {
+                    Value = new DateTime(2023, 1, 1),
+                    Granularity = UnitOfTimeGranularity.Year,
+                    Expected = (CalendarUnitOfTime)new CalendarYear(2022),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 12, 31),
+                    Granularity = UnitOfTimeGranularity.Year,
+                    Expected = (CalendarUnitOfTime)new CalendarYear(2022),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 1, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2022, QuarterNumber.Q4),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 1, 31),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2022, QuarterNumber.Q4),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 2, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2022, QuarterNumber.Q4),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 2, 28),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2022, QuarterNumber.Q4),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 3, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2022, QuarterNumber.Q4),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 3, 31),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2022, QuarterNumber.Q4),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 4, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q1),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 4, 30),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q1),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 5, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q1),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 5, 31),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q1),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 6, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q1),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 6, 30),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q1),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 7, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q2),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 7, 31),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q2),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 8, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q2),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 8, 30),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q2),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 9, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q2),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 9, 30),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q2),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 10, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q3),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 10, 31),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q3),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 11, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q3),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 11, 30),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q3),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 12, 1),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q3),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 12, 31),
+                    Granularity = UnitOfTimeGranularity.Quarter,
+                    Expected = (CalendarUnitOfTime)new CalendarQuarter(2023, QuarterNumber.Q3),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 1, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2022, MonthOfYear.December),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 1, 31),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2022, MonthOfYear.December),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 2, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.January),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 2, 28),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.January),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 3, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.February),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 3, 31),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.February),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 4, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.March),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 4, 30),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.March),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 5, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.April),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 5, 31),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.April),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 6, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.May),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 6, 30),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.May),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 7, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.June),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 7, 31),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.June),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 8, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.July),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 8, 30),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.July),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 9, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.August),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 9, 30),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.August),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 10, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.September),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 10, 31),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.September),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 11, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.October),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 11, 30),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.October),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 12, 1),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.November),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 12, 31),
+                    Granularity = UnitOfTimeGranularity.Month,
+                    Expected = (CalendarUnitOfTime)new CalendarMonth(2023, MonthOfYear.November),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 1, 1),
+                    Granularity = UnitOfTimeGranularity.Day,
+                    Expected = (CalendarUnitOfTime)new CalendarDay(2022, MonthOfYear.December, DayOfMonth.ThirtyOne),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 1, 1, 23, 59, 59),
+                    Granularity = UnitOfTimeGranularity.Day,
+                    Expected = (CalendarUnitOfTime)new CalendarDay(2022, MonthOfYear.December, DayOfMonth.ThirtyOne),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 1, 2),
+                    Granularity = UnitOfTimeGranularity.Day,
+                    Expected = (CalendarUnitOfTime)new CalendarDay(2023, MonthOfYear.January, DayOfMonth.One),
+                },
+                new
+                {
+                    Value = new DateTime(2023, 12, 31),
+                    Granularity = UnitOfTimeGranularity.Day,
+                    Expected = (CalendarUnitOfTime)new CalendarDay(2023, MonthOfYear.December, DayOfMonth.Thirty),
+                },
+            };
+
+            var expected = valueAndExpected.Select(_ => _.Expected).ToList();
+
+            // Act
+            var actual = valueAndExpected.Select(_ => _.Value.Previous(_.Granularity)).ToList();
+
+            // Assert
+            actual.AsTest().Must().BeEqualTo(expected);
         }
     }
 }
--- OBeautifulCode.AccountingTime/Logic/Extensions/DateTimeExtensions.cs
+++ OBeautifulCode.AccountingTime/Logic/Extensions/DateTimeExtensions.cs
@@ -7,6 +7,7 @@
 namespace OBeautifulCode.AccountingTime
 {
     using System;
+    using static System.FormattableString;
 
     /// <summary>
     /// Extension methods on <see cref="DateTime"/>.
@@ -24,6 +25,55 @@ namespace OBeautifulCode.AccountingTime
             this DateTime value)
         {
             var result = new CalendarDay(value.Year, (MonthOfYear)value.Month, (DayOfMonth)value.Day);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Finds a specified <see cref="CalendarUnitOfTime"/> prior to a reference date.
+        /// </summary>
+        /// <param name="value">The reference date.</param>
+        /// <param name="granularity">The granularity of the previous period.</param>
+        /// <returns>
+        /// Returns the <see cref="CalendarUnitOfTime"/> in the specified <paramref name="granularity"/>
+        /// that falls prior to the reference date.
+        /// </returns>
+        /// <exception cref="ArgumentException"><paramref name="granularity"/> is <see cref="UnitOfTimeGranularity.Unbounded"/>.</exception>
+        public static CalendarUnitOfTime Previous(
+            this DateTime value,
+            UnitOfTimeGranularity granularity)
+        {
+            if (granularity == UnitOfTimeGranularity.Unbounded)
+            {
+                throw new ArgumentOutOfRangeException(Invariant($"'{nameof(granularity)}' == '{UnitOfTimeGranularity.Unbounded}'"), (Exception)null);
+            }
+
+            CalendarUnitOfTime result;
+
+            if (granularity == UnitOfTimeGranularity.Year)
+            {
+                result = new CalendarYear(value.Year - 1);
+            }
+            else if (granularity == UnitOfTimeGranularity.Quarter)
+            {
+                result = value.Month <= 3
+                    ? new CalendarQuarter(value.Year - 1, QuarterNumber.Q4)
+                    : new CalendarQuarter(value.Year, (QuarterNumber)((value.Month - 1) / 3));
+            }
+            else if (granularity == UnitOfTimeGranularity.Month)
+            {
+                result = value.Month == 1
+                    ? new CalendarMonth(value.Year - 1, MonthOfYear.December)
+                    : new CalendarMonth(value.Year, (MonthOfYear)(value.Month - 1));
+            }
+            else if (granularity == UnitOfTimeGranularity.Day)
+            {
+                result = (CalendarDay)value.Date.ToCalendarDay().Plus(-1);
+            }
+            else
+            {
+                throw new NotSupportedException(Invariant($"This granularity is not supported: {granularity}."));
+            }
 
             return result;
         }
